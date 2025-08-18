10ebb8ed20d4b3e2952c754ab7fe7393967a1cd4
Jansen Ken Pegrasio <kenpegrasio@gmail.com>
2025-08-18 14:56:33 +0800

    refactor code

--- src/Server.cpp
+++ src/Server.cpp
@@ -31,6 +31,77 @@ Varint read_varint(std::ifstream& database_file, unsigned short ptr) {
   return Varint(cnt, ans);
 }
 
+unsigned short get_page_size(std::ifstream& database_file) {
+  database_file.seekg(16);  // moves the pointer to byte 16
+  char buffer[2];
+  database_file.read(buffer, 2);  // get byte 17 and 18
+  unsigned short page_size = (static_cast<unsigned char>(buffer[1]) |
+                              (static_cast<unsigned char>(buffer[0]) << 8));
+  return page_size;
+}
+
+unsigned short get_number_of_tables(std::ifstream& database_file) {
+  database_file.seekg(103);  // moves the pointer to byte 103
+  char buffer[2];
+  database_file.read(buffer, 2);  // get byte 104 and 105
+  unsigned short number_of_tables =
+      (static_cast<unsigned char>(buffer[1]) |
+       static_cast<unsigned char>(buffer[0]) << 8);
+  return number_of_tables;
+}
+
+unsigned char get_btree_page_type(std::ifstream& database_file,
+                                  int page_number) {
+  if (page_number == 1) {
+    database_file.seekg(100);  // pass the database header
+  } else {
+    database_file.seekg((page_number - 1) * 4096);
+  }
+  char btree_flag[1];
+  database_file.read(btree_flag, 1);  // get the b-tree page type
+  return static_cast<unsigned char>(btree_flag[0]);
+}
+
+int get_size_from_serial_type(unsigned long long serial_type) {
+  if (serial_type >= 0 && serial_type <= 4) {
+    return serial_type;
+  } else if (serial_type == 5) {
+    return 6;
+  } else if (serial_type == 6 || serial_type == 7) {
+    return 8;
+  } else if (serial_type == 8 || serial_type == 9) {
+    return 0;
+  } else if (serial_type == 10 || serial_type == 11) {
+    throw std::string("Serial type 10 or 11 is reserved");
+  } else if (serial_type >= 12 && serial_type % 2 == 0) {
+    return (serial_type - 12) / 2;
+  } else if (serial_type >= 13 && serial_type % 2 == 1) {
+    return (serial_type - 13) / 2;
+  } else {
+    throw std::string("Invalid serial type");
+  }
+}
+
+std::string get_schema_type(std::ifstream& database_file,
+                            std::map<std::string, int>& sqlite_schema,
+                            std::string identifier, int cell_location) {
+  std::vector<std::string> types = {"type", "name", "tbl_name", "rootpage",
+                                    "sql"};
+  int ptr = cell_location;
+  for (auto type : types) {
+    if (type == identifier) break;
+    ptr += sqlite_schema[type];
+  }
+  database_file.seekg(ptr);
+  std::string res = "";
+  for (int i = 0; i < sqlite_schema[identifier]; i++) {
+    char buffer[1];
+    database_file.read(buffer, 1);
+    res += buffer[0];
+  }
+  return res;
+}
+
 int main(int argc, char* argv[]) {
   // Flush after every std::cout / std::cerr
   std::cout << std::unitbuf;
@@ -55,47 +126,30 @@ int main(int argc, char* argv[]) {
       return 1;
     }
 
-    database_file.seekg(16);  // moves the pointer to byte 16
-
-    char buffer[2];
-    database_file.read(buffer, 2);  // get byte 17 and 18
-
-    unsigned short page_size = (static_cast<unsigned char>(buffer[1]) |
-                                (static_cast<unsigned char>(buffer[0]) << 8));
-
-    database_file.seekg(103);       // moves the pointer to byte 103
-    database_file.read(buffer, 2);  // get byte 104 and 105
-    unsigned short table_numbers = (static_cast<unsigned char>(buffer[1]) |
-                                    static_cast<unsigned char>(buffer[0]) << 8);
+    unsigned short page_size = get_page_size(database_file);
+    unsigned short number_of_tables = get_number_of_tables(database_file);
 
     std::cout << "database page size: " << page_size << std::endl;
-    std::cout << "number of tables: " << table_numbers << std::endl;
+    std::cout << "number of tables: " << number_of_tables << std::endl;
   } else if (command == ".tables") {
     std::ifstream database_file(database_file_path, std::ios::binary);
     if (!database_file) {
       std::cerr << "Failed to open the database file" << std::endl;
       return 1;
     }
-    database_file.seekg(100);  // pass the database header
-    char btree_flag[1];
-    database_file.read(btree_flag, 1);  // get the b-tree page type
-    if (static_cast<unsigned char>(btree_flag[0]) != 0x0d) {
+
+    if (get_btree_page_type(database_file, 1) != 0x0d) {
       throw std::string(
           "B-tree page type other than leaf table b-tree page is not "
           "supported!");
     }
-    // std::cout << "The B-tree page type is leaf table b-tree page" << std::endl;
 
-    char buffer[2];
-    database_file.seekg(103);       // moves the pointer to byte 103
-    database_file.read(buffer, 2);  // get byte 104 and 105
-    unsigned short table_numbers = (static_cast<unsigned char>(buffer[1]) |
-                                    static_cast<unsigned char>(buffer[0]) << 8);
-    // std::cout << "number of tables detected: " << table_numbers << std::endl;
+    unsigned short number_of_tables = get_number_of_tables(database_file);
 
     std::vector<unsigned short> cell_locations;
     database_file.seekg(108);
-    for (int i = 0; i < table_numbers; i++) {
+    for (int i = 0; i < number_of_tables; i++) {
+      char buffer[2];
       database_file.read(buffer, 2);
       unsigned short cell_ptr = (static_cast<unsigned char>(buffer[1]) |
                                  static_cast<unsigned char>(buffer[0]) << 8);
@@ -117,44 +171,16 @@ int main(int argc, char* argv[]) {
       int idx = 0;
       while (tot > 0) {
         Varint read = read_varint(database_file, ptr);
-        if (read.value >= 0 && read.value <= 4) {
-          sqlite_schema[schema_type[idx]] = read.value;
-        } else if (read.value == 5) {
-          sqlite_schema[schema_type[idx]] = 6;
-        } else if (read.value == 6 || read.value == 7) {
-          sqlite_schema[schema_type[idx]] = 8;
-        } else if (read.value == 8 || read.value == 9) {
-          sqlite_schema[schema_type[idx]] = 0;
-        } else if (read.value == 10 || read.value == 11) {
-          throw std::string("Serial type 10 or 11 is reserved");
-        } else if (read.value >= 12 && read.value % 2 == 0) {
-          sqlite_schema[schema_type[idx]] = (read.value - 12) / 2;
-        } else if (read.value >= 13 && read.value % 2 == 1) {
-          sqlite_schema[schema_type[idx]] = (read.value - 13) / 2;
-        } else {
-          throw std::string("Invalid serial type");
-        }
+        sqlite_schema[schema_type[idx]] = get_size_from_serial_type(read.value);
         ptr += read.bytes;
         tot -= read.bytes;
         idx++;
       }
-    //   std::cout << "SQLite Schema" << std::endl;
-    //   for (auto [key, value] : sqlite_schema) {
-    //     std::cout << key << ": " << value << std::endl;
-    //   }
-      ptr += sqlite_schema["type"] + sqlite_schema["name"];
-      database_file.seekg(ptr);
-      std::string table_name = "";
-      for (int i = 0; i < sqlite_schema["tbl_name"]; i++) {
-        char buffer[1];
-        database_file.read(buffer, 1);
-        table_name += buffer[0];
-      }
-    //   std::cout << "Table name detected: " << table_name << std::endl;
+      std::string table_name = get_schema_type(database_file, sqlite_schema, "tbl_name", ptr);
       table_names.push_back(table_name);
     }
     for (auto table_name : table_names) {
-        std::cout << table_name << " ";
+      std::cout << table_name << " ";
     }
     std::cout << std::endl;
   }
